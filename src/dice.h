5dde7a77980e3e896f5b53944485b0af40b81e8d
Jeremy M. Reed <reeje76@gmail.com>
2013-05-13 16:26:20 -0600

    License Details.

--- src/dice.h
+++ src/dice.h
@@ -5,6 +5,24 @@
 //Purpose: rolls an n-sided die.  The parameter n is given as input.
 //
 
+/*
+Copyright (C) yyyy  name of author
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 #if !defined __DICE_H
 #define __DICE_H
 
