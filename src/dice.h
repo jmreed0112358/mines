621f09c59771ab77f091527ff2cd79147c6c35cb
Jeremy M. Reed <reeje76@gmail.com>
2013-05-13 16:29:19 -0600

    License Details

--- src/dice.h
+++ src/dice.h
@@ -6,7 +6,7 @@
 //
 
 /*
-Copyright (C) yyyy  name of author
+Copyright (C) 2013 Jeremy Reed
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
